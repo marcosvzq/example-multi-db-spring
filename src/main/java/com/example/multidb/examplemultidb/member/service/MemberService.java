bbf334a0c4b3e6df909972f915a670c0fa9b54f5
Marcos Vzq <marcos.sis90@gmail.com>
2022-04-26 20:23:25 -0500

    added MemberResource and MemberService

--- src/main/java/com/example/multidb/examplemultidb/member/service/MemberService.java
+++ src/main/java/com/example/multidb/examplemultidb/member/service/MemberService.java
@@ -1,7 +1,26 @@
 package com.example.multidb.examplemultidb.member.service;
 
+import com.example.multidb.examplemultidb.member.model.Member;
+import com.example.multidb.examplemultidb.member.repository.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
+
+    private final Logger log = LoggerFactory.getLogger(MemberService.class);
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Page<Member> getAllMember(Pageable pageable){
+        log.debug("Enter service to getAllMember");
+        return memberRepository.findAll(pageable);
+    }
 }
---
+++ src/main/java/com/example/multidb/examplemultidb/web/rest/MemberResource.java
@@ -0,0 +1,37 @@
+package com.example.multidb.examplemultidb.web.rest;
+
+import com.example.multidb.examplemultidb.member.model.Member;
+import com.example.multidb.examplemultidb.member.service.MemberService;
+import com.example.multidb.examplemultidb.web.util.PaginationUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MemberResource {
+
+    private final Logger log = LoggerFactory.getLogger(MemberResource.class);
+
+    private final MemberService memberService;
+
+    public MemberResource(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<List<Member>> getAllMember(Pageable pageable){
+        log.debug("Enter REST to get a page of Member");
+        Page<Member> page = memberService.getAllMember(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "member");
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+}
